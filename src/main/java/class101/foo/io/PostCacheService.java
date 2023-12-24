0c1eed9f515d7d8f89ff9595eb4102c38b1baada
grace <eunjuzz@naver.com>
2023-12-25 00:58:29 +0900

    cache

---
+++ src/main/java/class101/foo/io/PostCacheService.java
@@ -0,0 +1,23 @@
+package class101.foo.io;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.scheduling.annotation.Scheduled;
+
+public class PostCacheService {
+
+    @Autowired
+    PostRepository postRepository;
+    private Page<Post> firstPostPage;
+
+    @Scheduled(cron="* * * * * *") //1초마다 갱신
+    public void updateFirstPostPage(){
+        firstPostPage = postRepository.findAll(PageRequest.of(0,20, Sort.by("id").descending()));
+    }
+
+    public Page<Post> getFirstPostPage(){
+        return this.firstPostPage;
+    }
+}
