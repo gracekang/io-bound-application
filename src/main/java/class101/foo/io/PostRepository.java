6c8accb0e7f63063fcb2c66fd371a3a4a450ca74
grace <eunjuzz@naver.com>
2023-12-26 00:03:38 +0900

    applying elk - 불필요어노테이션 제거

--- src/main/java/class101/foo/io/PostRepository.java
+++ src/main/java/class101/foo/io/PostRepository.java
@@ -1,7 +1,6 @@
 package class101.foo.io;
 
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
