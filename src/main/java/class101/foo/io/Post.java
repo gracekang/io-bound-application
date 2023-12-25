478b236cef9670e8c1a046e1baa0664b8b46b61a
grace <eunjuzz@naver.com>
2023-12-25 23:45:58 +0900

    applying elk

--- src/main/java/class101/foo/io/Post.java
+++ src/main/java/class101/foo/io/Post.java
@@ -1,15 +1,18 @@
 package class101.foo.io;
 
 import lombok.Data;
+import org.springframework.data.elasticsearch.annotations.Document;
 
 import javax.persistence.*;
 
 @Data
-@Entity(name = "post")
+//@Entity(name = "post")
+@Document(indexName = "post")
 public class Post {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "post_id_seq")
-    @SequenceGenerator(name="post_id_seq", sequenceName = "post_id_seq", allocationSize = 1)
-    private Long id;
+//    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "post_id_seq")
+//    @SequenceGenerator(name="post_id_seq", sequenceName = "post_id_seq", allocationSize = 1)
+//    private Long id;
+    private String id;
     private String content;
 }
